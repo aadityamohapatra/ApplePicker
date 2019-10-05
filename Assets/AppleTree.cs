c1cea54e775bcb474196110990011c5358beb8ae
Aaditya Mohapatra <aaditya.mohapatra@gmail.com>
2019-10-04 20:51:39 -0400

    Update Commit
    update to previous commit (includes scripts and prefabs and a little bit of the game working)

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -16,12 +16,35 @@ public class AppleTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed*Time.deltaTime;
+        transform.position = pos;
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        } 
+    }
+
+    private void FixedUpdate()
+    {
+        if(Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
